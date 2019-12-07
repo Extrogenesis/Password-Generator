58dd8dfe5de18ff85db7bd2d103fd2148ad5f7f1
Extrogenesis <extrogenesis@gmail.com>
2019-12-07 01:19:20 -0500

    init setup

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+  
+const HtmlWebPackPlugin = require('html-webpack-plugin')
+const {CleanWebpackPlugin} = require('clean-webpack-plugin')
+
+module.exports = {
+    mode: 'development',
+    entry: './src/index.js',
+    output: {
+        filename: 'main-[contentHash].js'
+    },
+    plugins: [new CleanWebpackPlugin(), 
+            new HtmlWebPackPlugin({
+                template: './src/index.htm'
+    })],
+    module: {
+        rules: [{
+            test: /\.js$/,
+            exclude: /node_modules/,
+            use: {
+                loader: 'babel-loader',
+                options: {
+                    presets: ['@babel/preset-env']
+                }
+            }
+        }]
+    }
+}
